95603a6fc932e666e8ade1cdb54baac70bf60d23
“yijunlin” <yijunlin_sjtu@126.com>
2024-01-31 15:45:08 +0000

    Working

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
+docker rm dockovpn
+
 docker run --cap-add=NET_ADMIN \
--v openvpn_conf:/opt/Dockovpn \
 -p 1194:1194/udp -p 80:8080/tcp \
--e HOST_ADDR=localhost \
---rm \
-alekslitvinenk/openvpn "$@"
+--name dockovpn alekslitvinenk/openvpn
